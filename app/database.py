619b14915435397343ddfd9a7dcb3592370be5a2
Mikhail Kulyagin <kulyaginmihail@yandex.ru>
2026-05-20 22:51:59 +0500

    Remove invalid ssl arg, keep retry logic

--- app/database.py
+++ app/database.py
@@ -12,7 +12,6 @@ engine = create_async_engine(
     echo=False,
     pool_size=5,
     max_overflow=10,
-    connect_args={"ssl": "require"},
 )
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 AsyncSessionFactory = AsyncSessionLocal
